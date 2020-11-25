e4eb6a9addb35fb6b7b510416fb74ffed51735b8
lukeludlow <lukeludlow@live.com>
2020-11-25 16:39:39 -0700

    Initial Commit

---
+++ tictactoe/tictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeApp.swift
+//  tictactoe
+//
+//  Created by luke ludlow on 2020-11-25.
+//
+
+import SwiftUI
+
+@main
+struct tictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
