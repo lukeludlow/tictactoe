d5757c7bebb0f661bb4d47db7b352c1c9f4f12a9
lukeludlow <lukeludlow@live.com>
2020-11-27 12:23:17 -0700

    ignore pods

--- Pods/Headers/Public/ToastUI/ToastUI-library-umbrella.h
+++
@@ -1,1 +0,0 @@
-../../../Target Support Files/ToastUI-library/ToastUI-library-umbrella.h
--- Pods/Target Support Files/ToastUI-framework/ToastUI-framework-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ToastUI_framework : NSObject
-@end
-@implementation PodsDummy_ToastUI_framework
-@end
